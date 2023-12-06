3c880a19c49cd0da56c4ff3a9e4db9a0da24da6d
Litzen Nicole T. Nisperos <nico@lgusuite.com>
2023-12-06 09:32:27 +0800

    transfer

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { BodyRoutingModule } from './body-routing.module';
 import { BodyComponent } from './body.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    BodyComponent
+    BodyComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
--- src/app/body/components/about/about.component.spec.ts
+++ src/app/body/components/about/about.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import { AboutComponent } from './about.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ AboutComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
